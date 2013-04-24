1960857ec2218a4b1371a5ae303dc9d163c732dc
xavier.galzin@gmail.com <xavier.galzin@gmail.com@c635af5a-d4cf-4164-0313-3d7d3c1945c6>
2013-04-24 00:47:03 +0000

    Working math reason why it was not working the variable declaration has to be done outside the main... the variable must not have the same name as the functions... to be tested in the main program...

--- trunk/MicroController/EE579WS/MathTest/main.c
+++ trunk/MicroController/EE579WS/MathTest/main.c
@@ -1,31 +1,35 @@
 /*
  * ======== Standard MSP430 includes ========
  */
-//#include <stdint.h>
 
+#include <stdint.h>
 #include <msp430.h>
-//#include <math.h>
+#include <math.h>
+#include <msp430g2231.h>
 
 
 /*
  *  ======== main ========
  */
+float x = 4.2f;
+float testTan=0.0f;
+float testSin =0.0f;
+float testCos = 0.0f;
 
 
 void main()
 {
 
-	float x = 4.2f;
 
+	  // Make P1.6 (green led) an output. SLAU144E p.8-3
+	  P1DIR |= BIT6;
+	  P1OUT=0xff;
 	while(1)
 	{
-
 		x -= 2.0f; // x = 2.2f now
-
-
-
+		testTan = atanf(x);
+		testSin = sinf(x);
+		testCos = cosf(x);
 
 	}
 }
-
-
