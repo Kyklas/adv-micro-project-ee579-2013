c012c3e323dcf56dae4fb234b26162e04dcc883b
stanislasbertrand@gmail.com <stanislasbertrand@gmail.com@c635af5a-d4cf-4164-0313-3d7d3c1945c6>
2013-04-29 21:18:56 +0000

    Comment added to the spi communication methods to communicate with the mouse.

--- trunk/MicroController/EE579WS/ToyCar/lib/modules/spi.h
+++ trunk/MicroController/EE579WS/ToyCar/lib/modules/spi.h
@@ -12,9 +12,25 @@
 
 #include <stdint.h>
 
+
+/*
+ * From the data sheet, defining the specified delay between operation
+ * SPI_DelayWtW : Delay from write to another write
+ * SPI_DelayWtR : Delay from write to a read
+ * The delais are hardcoded depending on the CPU clock speed
+ */
 void SPI_DelayWtW();
 void SPI_DelayWtR();
+
+/*
+ * Write operation : SPI_Write
+ * write the value to the register address.
+ */
 void SPI_Write(uint8_t addr,uint8_t value);
+/*
+ * Read operation : SPI_Read
+ * Read the value of the specified register
+ */
 uint8_t SPI_Read(uint8_t addr);
 
 #endif
