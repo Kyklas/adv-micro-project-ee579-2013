3fa84b8cd3d355243070b506eb4682e1e80f33da
stanislasbertrand@gmail.com <stanislasbertrand@gmail.com@c635af5a-d4cf-4164-0313-3d7d3c1945c6>
2013-04-29 21:18:56 +0000

    Comment added to the spi communication methods to communicate with the mouse.

--- MicroController/EE579WS/ToyCar/lib/modules/spi.c
+++ MicroController/EE579WS/ToyCar/lib/modules/spi.c
@@ -2,6 +2,12 @@
 #include <msp430.h>
 #include <spi.h>
 
+/*
+ * From the data sheet, defining the specified delay between operation
+ * SPI_DelayWtW : Delay from write to another write
+ * SPI_DelayWtR : Delay from write to a read
+ * The delais are hardcoded depending on the CPU clock speed
+ */
 
 void SPI_DelayWtW()
 {
@@ -15,6 +21,20 @@ void SPI_DelayWtR()
 	// CPU 12MHz => 240
 	__delay_cycles(300);
 }
+
+/*
+ * Write operation : SPI_Write
+ * write the value to the register address.
+ *
+ * the methode is timing sensitive
+ * - Disabling global interrups ( remembering the state )
+ * - Enable serial output ( multiplexing )
+ * - sending the address value
+ * - pulling interrupt to send the register value
+ * - sending value
+ * - wait until data sent
+ * - enable global interrupt re-enable.
+ */
 
 void SPI_Write(uint8_t addr,uint8_t value)
 {
@@ -32,6 +52,26 @@ void SPI_Write(uint8_t addr,uint8_t value)
 
 	__bis_SR_register(SR);
 }
+
+/*
+ * Read operation : SPI_Read
+ * Read the value of the specified register
+ * 
+ * the methode is timing sensitive
+ * - Disabling global interrups ( remembering the state )
+ * - Enable serial output ( multiplexing )
+ * - Clearing the receive flag from any previous reception
+ * - send the register address
+ * - pulling interrupt to send the register value
+ * - Clearing the receive flag
+ * - Disabling the serial output we can read the slave data
+ * - wait for the the slave to read the address
+ * - send dummy data so the clock is generated to get the slave data
+ * - wait for data to be receive 
+ * - re-enabling interrupts
+ * - return the receive buffer register.
+ */
+
 uint8_t SPI_Read(uint8_t addr)
 {
 	uint16_t SR = __get_SR_register() & GIE;
@@ -42,6 +82,7 @@ uint8_t SPI_Read(uint8_t addr)
 	// enable
 	P1SEL |= BIT7;
 	P1SEL2 |= BIT7;
+	// Clearing the receive flag from any previous reception
 	UC0IFG &= ~UCB0RXIFG;
 	__delay_cycles(6);
 	UCB0TXBUF = addr&0x7F;				// Send address
