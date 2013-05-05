89323a1c8ee9a65bb20cf990b81905c73be52ba4
stanislasbertrand@gmail.com <stanislasbertrand@gmail.com@c635af5a-d4cf-4164-0313-3d7d3c1945c6>
2013-05-05 19:21:36 +0000

    Initial version for song gimmicks with Portal Still Alive and Mario theme songs. The timer0 should be setup before with 1500khz clock ( 12 MHz/8 )

---
+++ MicroController/EE579WS/ToyCar/lib/modules/beep.h
@@ -0,0 +1,16 @@
+
+
+#ifndef BEEP_H
+#define BEEP_H
+
+
+extern const int portal[];
+
+void startbeep(int frequency);
+void stopbeep();
+
+void playportal();
+
+void playmario();
+
+#endif // BEEP_H
